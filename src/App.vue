2578de3fb06288b6e7c1e225a437295f18faa67f
mgrru <ru@fufu.moe>
2024-06-08 19:39:41 +0800

    修改下token的保存方式，确保刷新网页时不会丢失token

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import router from './router'
-import { onMounted } from 'vue'
+  import { RouterView } from 'vue-router'
+  import router from './router'
+  import { onMounted } from 'vue'
+  import axios from 'axios'
 
-const homePage = () => {
-  router.push('/login')
-}
+  const homePage = () => {
+    router.push('/login')
+  }
 
-onMounted(() => {
-  homePage()
-})
+  const getToken = () => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      axios.defaults.headers.common[ 'Authorization' ] = token
+    }
+  }
+
+  onMounted(() => {
+    homePage()
+    getToken()
+  })
 </script>
 
 <template>
